c44c1350c0623d587854503d44eec10e3ff13618
Eric Santos <eric.silvasantos44@gmail.com>
2022-01-25 20:30:41 -0300

    Adicionado o quarto requisito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,13 +1,13 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return data.employees.some((employee) => employee.managers.includes(id) )
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  if(isManager(managerId)){
-    const arrayAux = data.employees.filter((employee) => employee.managers.includes(managerId))
-    return arrayAux.map((funcionario) => `${funcionario.firstName} ${funcionario.lastName}`)
+  if (isManager(managerId)) {
+    const arrayAux = data.employees.filter((employee) => employee.managers.includes(managerId));
+    return arrayAux.map((funcionario) => `${funcionario.firstName} ${funcionario.lastName}`);
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
