85e1254c4d0b7ed6214704c607a5d6cc9f1d91f6
Eric Santos <eric.silvasantos44@gmail.com>
2022-01-25 15:41:41 -0300

    Adicionado o priemiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código here
+function getSpeciesByIds(...ids) {
+  const arraySpecies = data.species.filter((specie) => ids.find((id) => specie.id === id));
+  return arraySpecies;
 }
-
 module.exports = getSpeciesByIds;
