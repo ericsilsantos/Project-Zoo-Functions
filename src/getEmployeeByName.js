30910533d6101b1443aa2e9fb2c7eb0776fd9d25
Eric Santos <eric.silvasantos44@gmail.com>
2022-01-25 16:18:14 -0300

    Adicionado o terceito requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees.find((em) => em.firstName === employeeName || em.lastName === employeeName);
 }
-
 module.exports = getEmployeeByName;
