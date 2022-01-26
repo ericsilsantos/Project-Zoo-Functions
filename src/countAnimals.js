4832c8300433cd6740ba7020a605c406f1b00faa
Eric Santos <eric.silvasantos44@gmail.com>
2022-01-25 21:42:39 -0300

    Adicionado o quinto requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function totalAnimalsForSpecie(animal) {
+  let contador = 0;
+  species.forEach((specie) => {
+    if (animal.specie === specie.name) {
+      if (animal.sex) {
+        contador = specie.residents.filter((resident) => resident.sex === animal.sex).length;
+        return contador;
+      }
+      contador += specie.residents.length;
+    }
+  });
+  return contador;
 }
 
+function countAnimals(animal) {
+  if (!animal) {
+    const objAnimals = {};
+    species.forEach((specie) => {
+      objAnimals[specie.name] = specie.residents.length;
+    });
+    return objAnimals;
+  }
+  return totalAnimalsForSpecie(animal);
+}
+// console.log(countAnimals())
 module.exports = countAnimals;
