f8e2b615c0e5f22bd03959dc11bd27906b21b7d8
Eric Santos <eric.silvasantos44@gmail.com>
2022-01-26 13:32:03 -0300

    Adiconado o sexto requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,34 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const totalEntrants = {
+    adult: 0,
+    child: 0,
+    senior: 0,
+  };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      totalEntrants.child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      totalEntrants.adult += 1;
+    } else {
+      totalEntrants.senior += 1;
+    }
+  });
+  return totalEntrants;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const totalPeoples = countEntrants(entrants);
+  const payAdult = totalPeoples.adult * prices.adult;
+  const payChild = totalPeoples.child * prices.child;
+  const paySenior = totalPeoples.senior * prices.senior;
+  return payAdult + payChild + paySenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
