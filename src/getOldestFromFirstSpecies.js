79ad8496705c3728146ff4816f34cae6a568c04e
Eric Santos <eric.silvasantos44@gmail.com>
2022-01-27 15:22:49 -0300

    Adiconado o nono requisito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+const { species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = employees.find((employee) => employee.id === id);
+  const residents = (species
+    .find((specie) => specie.id === funcionario.responsibleFor[0]).residents)
+    .sort((rA, rB) => rB.age - rA.age);
+  return [residents[0].name, residents[0].sex, residents[0].age];
 }
 
 module.exports = getOldestFromFirstSpecies;
